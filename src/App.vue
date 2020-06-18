0327cdfcb292221e7ec242d4d31789039c017134
MateusBraz <silvabraz96@gmail.com>
2020-06-18 18:42:07 -0400

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <BarraAplicativo />
+    <v-main class="background-color = blue-grey darken-4">
+      <v-container fluid>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import BarraAplicativo from "./components/BarraAplicativo.vue";
+export default {
+  name: "App",
+
+  components: {
+    BarraAplicativo
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
+
+<style>
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+    opacity: 1;
   }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 0.3s ease;
+}
+
+.slide-leave-active {
+  animation: slide-out 0.3s ease;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-card dark class="mt-10">
+    <v-alert color="blue-grey" dark dense icon="mdi-school" prominent>Projeto de Estágio.</v-alert>
+  </v-card>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
